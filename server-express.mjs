e3c6e37bc1f85f5defdd48fa3c56cbb799ed3f0b
PinkHam <alexandrejamroz@gmail.com>
2024-09-30 16:59:46 +1100

    Commit 6

--- server-express.mjs
+++ server-express.mjs
@@ -10,14 +10,16 @@ if (app.get("env") === "development") {
   app.use(morgan("dev"));
 }
 
+app.set("view engine", "ejs");
+
 app.use(express.static("static"));
 
 app.get("/random/:nb", async function (request, response, next) {
   const length = request.params.nb;
-  const contents = Array.from({ length })
-    .map((_) => `<li>${Math.floor(100 * Math.random())}</li>`)
-    .join("\n");
-  return response.send(`<html><ul>${contents}</ul></html>`);
+  const numbers = Array.from({ length }, () => Math.floor(100 * Math.random()));
+  const welcome = "Bienvenue !"
+
+  return response.render("random", { numbers, welcome });
 });
 
 
